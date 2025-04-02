a2657776abf70a32e1d74499343d1db1d1849d9f
Gustavo <166347161+Gustavomw1@users.noreply.github.com>
2025-04-02 16:40:59 -0300

    Tipos primitivos

--- basico/comando.java
+++ basico/comando.java
@@ -1,5 +1,7 @@
+package basico;
+
 public class comando {
     public static void main(String[] args) {
-        System.out.println("KA-ME-HA-ME-HAAAA");
+        System.out.println("KA-ME-HA-ME-HA");
     }
 }
---
+++ basico2/tiposPrimitivos.java
@@ -0,0 +1,21 @@
+package basico2;
+
+public class tiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int age = 10;
+
+        /*
+         * long numeroGrande = 10000L;
+         * double salarioDouble = 2000.0;
+         * float salarioFloat = 2500.0f;
+         * byte idadeByte = -128;
+         * short idadeShort = 10;
+         * boolean verdadeiro = true;
+         * boolean falso = false;
+         * char caractere = 'A';
+         */
+
+        System.out.println("A idade é " + age);
+    }
+}
