ce838385f3f3e93f016b6687818d5c52f7eb172d
Gustavo <166347161+Gustavomw1@users.noreply.github.com>
2025-04-03 19:31:59 -0300

    exercicio resolvido

---
+++ aulas/estruturasCondicionais/ExercicioEstruturasCondicionais.java
@@ -0,0 +1,17 @@
+package aulas.estruturasCondicionais;
+
+public class ExercicioEstruturasCondicionais {
+    // Desafio com && (e)
+    public static void main(String[] args) {
+        int money = 1200;
+        String conta = "ativa";
+
+        if (money >= 1200 && conta == "ativa") {
+            System.out.println("Você consegue comprar esse produto.");
+        } else {
+            System.out.println("Você nao pode compra esse produto.");
+        }
+
+        // Desafio com || (ou)
+    }
+}
