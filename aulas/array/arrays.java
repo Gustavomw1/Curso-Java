a02593a812b5c9678e8cd581ab26b7c94fa3d843
Gustavo <166347161+Gustavomw1@users.noreply.github.com>
2025-04-06 15:24:00 -0300

    new aulas sobre arrays

---
+++ aulas/array/arrays.java
@@ -0,0 +1,12 @@
+package aulas.array;
+
+public class arrays {
+    public static void main(String[] args) {
+        int[] idades = new int[3];
+        idades[0] = 21;
+        idades[1] = 15;
+        idades[2] = 28;
+        System.out.println(idades[1]);
+
+    }
+}
---
+++ aulas/array/arraysforeach.java
@@ -0,0 +1,14 @@
+package aulas.array;
+
+public class arraysforeach {
+    public static void main(String[] args) {
+        int[] numeros = new int[3];
+        int[] numeros2 = { 1, 2, 3, 4, 5 };
+        int[] numeros3 = new int[] { 1, 2, 3, 4, 5 };
+
+        for (int i = 0; i < numeros2.length; i++) {
+            System.out.println(numeros3[i]);
+        }
+    }
+
+}
